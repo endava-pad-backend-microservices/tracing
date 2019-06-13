7376d2521b6a67af898054186049fc26473c4575
ayancovich <ayancovich@endava.com>
2019-06-13 14:08:56 -0300

    Dockerized tracing ms

--- src/main/java/com/endava/tracing/TracingApplication.java
+++ src/main/java/com/endava/tracing/TracingApplication.java
@@ -2,8 +2,12 @@ package com.endava.tracing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
@@ -11,6 +15,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class TracingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TracingApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+		}
 	}
 
 }
